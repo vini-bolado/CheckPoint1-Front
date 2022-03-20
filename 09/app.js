83af8b5b73f342001e6de463113d7ec7ee75bba9
vinicius evandro <vinnievandro@hotmail.com>
2022-03-20 16:50:18 -0300

    Vinicius terminado

--- 09/app.js
+++ 09/app.js
@@ -1,6 +1,7 @@
 
 // // Johnny - 1 e 1.1
-// let botao = document.querySelector("button");
+
+
 // let btn = true;
 // let sidebar = document.querySelector("div.l-sidebar");
 
@@ -14,20 +15,62 @@
 //!-------------------------------------------------------------------------------------
 //*vinicius
 
+
+
 var documento = document.body;
-var tela = document.querySelector("#telaInicial");
+var sideBar = document.querySelector(".l-sidebar");
+
+//! 1 parte
+
+function sideB(){
+
+    sideBar.classList.add('l-sidebar--close')
+    
+}
+
+sideB()
+
+//! 2 parte
+function AbrirFechar(){
+
+    var auxiliar = sideBar.classList[1]
+
+    if (auxiliar == 'l-sidebar--close'){
+
+       sideBar.classList.remove('l-sidebar--close')
+       sideBar.classList.add('l-sidebar--open')
+
+    }else {
+
+       sideBar.classList.remove('l-sidebar--open')
+       sideBar.classList.add('l-sidebar--close')
+
+    }
+
+   }
+
+
 
 document.addEventListener("keydown", (evento) => {
 
     if(evento.code == "Space"){
 
-    tela.style.display = "none"
+        AbrirFechar()
 
-}
-
-        
+}})
 
 
+    
+   
+var menuButton = document.getElementById('l-sidebar__btn');
+
+menuButton.addEventListener("click", (evento) => {
+    console.log(evento)
+});
+ 
+
+
+    
 
 
 
@@ -35,38 +78,38 @@ document.addEventListener("keydown", (evento) => {
 
 
 //!-------------------------------------------------------------------------------------
-// 3 Rafael 
+//3 Rafael 
 //selecionar os elementos; criar rotina(função) e puxar evento do usuário 
 
-        // var abrirMenu = document.getElementById('abrirMenu');
+        var abrirMenu = document.getElementById('abrirMenu');
 
-        // function habilitarSidebar() {  
+        function habilitarSidebar() {  
 
-        //     //alterei a linha 4 (button) para ha(desa)bilitar o "disable" ??
-        //     //*tentar criar o disable pelo JS e não colocando no HTML
+            //alterei a linha 4 (button) para ha(desa)bilitar o "disable" ??
+            //*tentar criar o disable pelo JS e não colocando no HTML
 
-        //     if(evento.code = "dbClick")
+            if(evento.code = "dbClick")
 
-        //     sidebar.style.display = "none"; 
+            sidebar.style.display = "none"; 
 
-        //         else(evento.code = "dbClick")
+                else(evento.code = "dbClick")
 
-        //     sidebar.style.display == "block"; 
+            sidebar.style.display == "block"; 
 
-        //     //Adicionar atributo
-        //     abrirMenu.setAttribute("disable", "false")
-        // }   
-        //     //Remove atributo
-        //     abrirMenu.setAttribute("disable");
+            //Adicionar atributo
+            abrirMenu.setAttribute("disable", "false")
+        }   
+            //Remove atributo
+            abrirMenu.setAttribute("disable");
 
 
-        // // util??
+        // util??
         
-        //     e.addEventListener('dblclick', function(evento) {
-        //         e.innerText = 'sidebarMenu';
+            e.addEventListener('dblclick', function(evento) {
+                e.innerText = 'sidebarMenu';
 
 
-        // })
+        })
 
 
 
