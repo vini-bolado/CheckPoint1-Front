ecf254b434e5d1b5f2b3207963c6f702ed4070f1
johnnyrps <johnnyrps@gmail.com>
2022-03-20 23:27:57 -0300

    johnny

--- 09/app.js
+++ 09/app.js
@@ -1,96 +1,56 @@
-//*vinicius
+// 1 - Johnny
+documento = document.body;
+var Button = document.getElementById('l-sidebar__btn');
+var sideBar = document.getElementById('l-sidebar');
 
 
-var documento = document.body;
-var sideBar = document.querySelector(".l-sidebar");
 
-//! 1 parte
+//1.1 - Johnny
+
+Button.addEventListener("click", AbrirFechar);
+sideBar.addEventListener("click", AbrirFechar);
+
+
+
+// 2 parte - Vinicius
 
 function sideB(){
 
-    sideBar.classList.add('l-sidebar--close')
-    
+sideBar.classList.add('l-sidebar--close')
+
 }
 
 sideB()
 
-//1.1 - Johnny
-var btn = document.querySelectorAll("button#l-sidebar__btn");
-
-document.addEventListener("click", (evento) => {
-
-   evento = AbrirFechar('btn');
-
-});
-
-
-
-//! 2 parte
-
 function AbrirFechar(){
 
-    var auxiliar = sideBar.classList[1]
+var auxiliar = sideBar.classList[1]
 
     if (auxiliar == 'l-sidebar--close'){
 
-       sideBar.classList.remove('l-sidebar--close')
-       sideBar.classList.add('l-sidebar--open')
-
+sideBar.classList.remove('l-sidebar--close')
+sideBar.classList.add('l-sidebar--open')
+    
     }else{
 
-       sideBar.classList.remove('l-sidebar--open')
-       sideBar.classList.add('l-sidebar--close')
+sideBar.classList.remove('l-sidebar--open')
+sideBar.classList.add('l-sidebar--close')
 
-    }
+}
 
-   }
+};
 
-
-document.addEventListener("keydown", (evento) => {
-
-    if(evento.code == "Space"){
-
+document.addEventListener("keydown", (key) => { 
+    if (key.code == "Space") {
         AbrirFechar()
+    }
+})
 
-}})
-
-
-    
-
-
-
-    
 
 //!-------------------------------------------------------------------------------------
-//3 Rafael 
-//selecionar os elementos; criar rotina(função) e puxar evento do usuário 
+//3 - Johnny
+document.addEventListener("dblclick", AbrirFechar);
 
-        var abrirMenu = document.getElementById('abrirMenu');
-
-        function habilitarSidebar() {  
-
-        
-            //*tentar criar o disable pelo JS e não colocando no HTML
-
-            if(evento.code = "dbClick")
-
-            sidebar.style.display = "none"; 
-
-                else(evento.code = "dbClick")
-
-            sidebar.style.display == "block"; 
-
-            //Adicionar atributo
-            abrirMenu.setAttribute("disable", "false")
-        }   
-            //Remove atributo
-            abrirMenu.setAttribute("disable");
+// 4 - Todos ---------------------------------------------------------------------------
 
 
-        // util??
-        
-            e.addEventListener('dblclick', function(evento) {
-                e.innerText = 'sidebarMenu';
-
-
-        });
